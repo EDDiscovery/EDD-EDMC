d9e3faa99278959ae2b806af0a26fc7db6b4dad8
robbyxp1 <robxp@hotmail.com>
2020-07-17 14:05:06 +0100

    Updated build name

---
+++ installer/build.bat
@@ -0,0 +1,9 @@
+if "%1" == "" (
+set /P vno=Version Number (10.1.2 etc) :
+) else (
+echo set to %1
+set vno=%1
+)
+echo Build %vno%
+"\Program Files (x86)\Inno Setup 6\iscc.exe" /DMyAppVersion=%vno% innoscript.iss
+certutil -hashfile installers\EDD-EDMC-%vno%.exe SHA256 >installers\checksums.%vno%.txt
